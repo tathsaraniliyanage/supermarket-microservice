23a1e5afc5a64160bdf9f0081c6e539829a3cfd1
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2025-03-02 19:40:40 +0530

    python file updated

--- customer-service/customer_service.py
+++ customer-service/customer_service.py
@@ -16,11 +16,22 @@
 
 
 from flask import Flask, jsonify
+import py_eureka_client.eureka_client as eureka_client
 
+# pip install flask py_eureka_client
+
+# create flask application
 app = Flask(__name__)
 
 CONTEXT_PATH = '/customer-service'
 SERVICE_PORT = 5000
+EUREKA_SERVICE = "http://localhost:8761/eureka/"
+
+eureka_client.init(
+    eureka_server=EUREKA_SERVICE,
+    app_name="CUSTOMER_SERVICE",
+    instance_port=SERVICE_PORT
+)
 
 @app.route(f'{CONTEXT_PATH}/customers', methods=['GET'])
 def get_customers():
